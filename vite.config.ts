aab4fdfba9ba5c02f1b291c4b87db6c43c986859
xomaaz <su06942@st.habib.edu.pk>
2025-10-27 08:58:54 +0500

    add vecel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1000
+  },
+  server: {
+    port: 3000,
+  }
 })
